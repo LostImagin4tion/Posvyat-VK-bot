0336aa626461f8df39feb789f8f7e21530943a54
Элиза <effazylova@edu.hse.ru>
2022-08-14 23:05:51 +0300

    fix: database

--- app/bot.py
+++ app/bot.py
@@ -1,5 +1,6 @@
 import vk_api
 import psycopg2
+from sqlalchemy.testing import db
 
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
@@ -11,7 +12,7 @@ from vk_config import token_vk
 from create_db import engine, get_session, guests, orgs, groups, info, tech_support, sendings
 from sqlalchemy.orm import Session, sessionmaker
 
-session = sessionmaker(bind=engine)
+session = Session(bind=engine)
 
 vk_session = vk_api.VkApi(token=token_vk)
 
@@ -47,10 +48,6 @@ def start():
             user_id = event.user_id
             text = event.text.lower()
 
-            user = vk_session.method("users.get", {"user_ids": user_id})
-            first_name = user[0]['first_name']
-            last_name = user[0]['last_name']
-
 
             if text == "start":
 
@@ -59,24 +56,34 @@ def start():
                     send_message(vk_session, user_id, "Hi, admin!")
                 else:
                     send_message(vk_session, user_id, "Hi, user!")
-                    #тут должно выделяться новое поле в бд для гостей
 
                 result = vk_session.method("groups.isMember", {"group_id": group_id[0]["id"], 'user_id': user_id})
 
                 if result:
                     send_message(vk_session, user_id, "Спасибо, что вступили в наше сообщество!")
                     #в колонке с вступили надо отметить тру
-                    i = vk_session.query(guests).get(8)
-                    i.first_group = True
-                    session.add(i)
+                    user_setting = session.query(guests).filter_by(id=user_id).first()
+
+                    if user_setting:
+                        user_setting.first_group = True
+                        user_setting.second_group = True
+                    else:
+                        user_setting = guests(id=user_id, first_group=True, percent=True)
+                        session.add(user_setting)
                     session.commit()
+
                 else:
                     send_message(vk_session, user_id, "Вы не вступили в сообщество.")
-                    #в колонке с вступили надо отметить фолс
-                    i = vk_session.query(guests).get(8)
-                    i.second_group = False
-                    session.add(i)
+                    user_setting = session.query(guests).filter_by(id=user_id).first()
+
+                    if user_setting:
+                        user_setting.first_group = True
+                        user_setting.second_group = False
+                    else:
+                        user_setting = guests(id=user_id, first_group=True, second_group=False)
+                        session.add(user_setting)
                     session.commit()
+
 
 
 
